de84cee3ab924e4ab1a140384619e88033ddf54d
Grace <neguekwahammaelgrace@gmail.com>
2025-01-27 18:16:21 +0100

    destination page done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,18 @@
+"use client"
+import { useState } from "react";
 import Navbar from "../../../components/Navbar";
 import Image from "next/image";
+import data from "../data/data.json";
 
 const About = () => {
+  const [activeTab, setActiveTab] = useState("Moon");
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
+  const activeData = data.destinations.find(dest => dest.name.toLowerCase() === activeTab.toLowerCase());
+
   return (
     <div className="h-screen bg-cover bg-center bg-fixed bg-[url(/assets/destination/background-destination-desktop.jpg)]">
       <Navbar />
@@ -12,34 +23,35 @@ const About = () => {
         <div className="flex justify-evenly text-white mt-10 ml-40 mr-40 items-center font-serif">
           <div>
             <Image
-              src="/assets/destination/image-mars.png"
+              src={activeData.images.png}
               alt="moon-image"
               width={400}
               height={400}
             />
           </div>
           <div className="flex flex-col space-y-5 w-1/3 h-full justify-between">
-            <ul className="flex space-x-4 text-sm text-slate-500">
-              <li>MOON</li>
-              <li>MARS</li>
-              <li>EUROPA</li>
-              <li>TITAN</li>
+            <ul className="destination-links flex space-x-4 text-sm text-slate-500">
+              {data.destinations.map((dest) => (
+                <li
+                  key={dest.name}
+                  onClick={() => handleTabClick(dest.name)}
+                  className={`relative text-white ${activeTab === dest.name ? "active" : ""}`}
+                >
+                  {dest.name.toUpperCase()}
+                </li>
+              ))}
             </ul>
-            <h1 className="text-8xl">MARS</h1>
-            <p className="text-slate-500">
-              Don’t forget to pack your hiking boots. You’ll need them to tackle
-              Olympus Mons, the tallest planetary mountain in our solar system.
-              It’s two and a half times the size of Everest!
-            </p>
+            <h1 className="text-8xl">{activeData.name.toUpperCase()}</h1>
+            <p className="text-slate-500">{activeData.description}</p>
             <hr />
             <div className="flex justify-between">
               <div>
                 <p className="text-sm text-slate-500">AVG DISTANCE</p>
-                <h5 className="text-2xl">225 MIL. KM</h5>
+                <h5 className="text-2xl">{activeData.distance}</h5>
               </div>
               <div>
                 <p className="text-sm text-slate-500">EST. TRAVEL TIME</p>
-                <h5 className="text-2xl">9 MONTHS</h5>
+                <h5 className="text-2xl">{activeData.travel}</h5>
               </div>
             </div>
           </div>
