e3a60834cf37c552c38fd2f361d63df1d93f6b75
Grace <neguekwahammaelgrace@gmail.com>
2025-01-28 12:11:28 +0100

    technology page done

--- src/app/technology/page.tsx
+++ src/app/technology/page.tsx
@@ -1,17 +1,19 @@
-"use client"
+"use client";
 import Navbar from "../../../components/Navbar";
 import Image from "next/image";
 import data from "../data/data.json";
 import { useState } from "react";
 
 const Technology = () => {
-  const [activeTab, setActiveTab] = useState("Moon");
+  const [activeTab, setActiveTab] = useState(1);
 
-  const handleTabClick = (tab: string) => {
+  const handleTabClick = (tab: number) => {
     setActiveTab(tab);
   };
 
-  const activeData = data.technology.find(tech => tech.name.toLowerCase() === activeTab.toLowerCase());
+  const activeData = data.technology.find(
+    (tech) => tech.id === activeTab
+  );
 
   if (!activeData) {
     return <div>Loading...</div>;
@@ -25,25 +27,27 @@ const Technology = () => {
         </h1>
         <div className="flex space-x-5 text-white ml-40 items-center justify-evenly font-serif">
           <div className="flex flex-col items-start space-y-7">
-            <div className="w-10 h-10 rounded-full bg-muted border items-center flex justify-center text-white">
-              1
-            </div>
-            <div className="w-10 h-10 rounded-full bg-muted border items-center flex justify-center text-white">
-              2
-            </div>
-            <div className="w-10 h-10 rounded-full bg-muted border items-center flex justify-center text-white">
-              3
-            </div>
+            {data.technology.map((tech) => (
+              <div
+                key={tech.id}
+                onClick={() => handleTabClick(tech.id)}
+                className={`w-10 h-10 rounded-full bg-muted border items-center flex justify-center text-white ${
+                  activeTab === tech.id ? "bg-white text-slate-900" : ""
+                }`}
+              >
+                {tech.id}
+              </div>
+            ))}
           </div>
           <div className="flex flex-col space-y-5 w-1/2 justify-between">
             <p className="text-slate-300">THE TERMINOLOGY...</p>
-            <h1 className="text-4xl">{activeData.name}</h1>
+            <h1 className="text-4xl">{activeData.name.toUpperCase()}</h1>
             <p className="text-slate-300">{activeData.description}</p>
           </div>
           <div>
             <Image
-              src={activeData.images.landscape}
-              alt="moon-image"
+              src={activeData.images.portrait}
+              alt={activeData.name}
               width={400}
               height={400}
             />
