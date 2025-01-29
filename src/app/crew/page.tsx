7ceada58b2f42f2d70d2bd8b101caf9487b508dc
Grace <neguekwahammaelgrace@gmail.com>
2025-01-29 12:40:27 +0100

    responsive crew page done

--- src/app/crew/page.tsx
+++ src/app/crew/page.tsx
@@ -21,13 +21,15 @@ const Crew = () => {
           <span className="mr-5 text-gray-500">02</span>MEET YOUR CREW
         </h1>
         <div className="flex flex-col sm:flex sm:flex-row space-x-8 text-white mt-10 sm:ml-30 sm:mr-30 text-center sm:text-left items-center justify-evenly font-serif">
-          <div className="flex flex-col space-y-5 sm:w-1/3 h-full justify-between">
-            <h2 className="text-slate-300 text-2xl">
-              {crew[activeIndex].role}
-            </h2>
-            <h1 className="text-5xl">{crew[activeIndex].name}</h1>
-            <p className="text-slate-300">{crew[activeIndex].bio}</p>
-            <div className="flex items-center space-x-4 pt-20 ml-40 sm:ml-0">
+          <div className="flex flex-col sm:w-1/3 h-full order-2 justify-between">
+            <div className="space-y-5 order-3 sm:order-2">
+              <h2 className="text-slate-300 text-2xl">
+                {crew[activeIndex].role}
+              </h2>
+              <h1 className="text-5xl">{crew[activeIndex].name}</h1>
+              <p className="text-slate-300 ml-10 mr-10 sm:text-left sm:ml-0 sm:mr-0">{crew[activeIndex].bio}</p>
+            </div>
+            <div className="flex items-center space-x-4 pt-10 pb-10 ml-40 sm:ml-0">
               {crew.map((_, index) => (
                 <button
                   key={index}
@@ -39,12 +41,12 @@ const Crew = () => {
               ))}
             </div>
           </div>
-          <div className="w-64 h-64 relative sm:w-96 sm:h-96 top-0 flex items-center">
+          <div className="w-64 h-64 relative order-1 sm:order-3 sm:w-96 sm:h-96 ">
             <Image
               src={crew[activeIndex].images.png}
               alt={crew[activeIndex].name}
               layout="fill"
-              objectFit="contain"
+              objectFit="cover"
               className="z-0"
             />
           </div>
